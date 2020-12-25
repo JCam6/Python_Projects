dbbe484e7d226f81476240cb0acc014624d4a53e
JCam6 <jcamero6@gmail.com>
2020-12-25 15:47:51 -0500

    Update

--- FibonacciSq.py
+++ FibonacciSq.py
@@ -2,13 +2,12 @@
 # System recursion limit will bound program response near a 130 digit integer.
 
 def main():
-
-
     position = int(input("What posiiton in Fibonacci's sequence would you like to find? "))
-    result = fib(position)  
-    print(result) 
+    result = fib(position)
+    print(result)
 
-def fib(n, m = {}):
+
+def fib(n, m={}):
     if n in m:
         return m[n]
     else:
@@ -17,7 +16,6 @@ def fib(n, m = {}):
         else:
             m[n] = fib(n - 1, m) + fib(n - 2, m)
             return m[n]
-
 
 
 main()
