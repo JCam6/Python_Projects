47b6bfe7debc205c97a8a6c7291722213bc00775
JCam6 <jcamero6@gmail.com>
2020-12-25 15:54:11 -0500

    Update

--- GolfScoreBoard.py
+++ GolfScoreBoard.py
@@ -1,12 +1,11 @@
 # The second program will read the records from the golf.txt file 
 # and display them with appropriate headings above the data being displayed.
 
-#-----------------------------------------------------------------------------------------------------------------------------------------------------#
-# Author: JM Cameron
-# Date: November 2020
+# Author: JM Cameron Date: November 2020
 
 # Importing the csv package to use the csv.reader function.
 import csv
+
 
 # This is the main function of the program.
 def main():
@@ -18,17 +17,17 @@ def main():
         try:
 
             # This step opens and reads the records from golf.txt.
-            with open('golf.txt','r', encoding='ascii', newline='\n') as f:
-                
+            with open('golf.txt', 'r', encoding='ascii', newline='\n') as f:
+
                 # This line prints a formatted header row for the program.
-                print('\n','-'*20,"Country Club Tournament Results",'-'*20,'\n')            
-                
+                print('\n', '-' * 20, "Country Club Tournament Results", '-' * 20, '\n')
+
                 # This step prints headings for the data being displayed.
-                header = ["First Name","Last Name","Handicap","Score","Result"]
+                header = ["First Name", "Last Name", "Handicap", "Score", "Result"]
                 for column in header:
-                    print(fixed_length(column,18), end= "")
+                    print(fixed_length(column, 18), end="")
                 print()
-            
+
                 # This step formats the data from golf.txt to display in a table according to the headings above.
                 reader = csv.reader(f)
                 data = []
@@ -36,9 +35,9 @@ def main():
                     data.append(row)
                 for row in data:
                     for column in row:
-                        print(fixed_length(column,18), end= "")
+                        print(fixed_length(column, 18), end="")
                     print()
-        
+
         # This step closes the exception handler.
         except:
             print("\nThe tournament data has not been entered yet. Please run that program first!")
@@ -48,19 +47,21 @@ def main():
         repeat = input("\nPlease enter 'yes' if you would like to display the tournament results again: ")
 
     # This line prints a formatted footer row for the program.
-    print('\n','-'*36,"Goodbye",'-'*36,'\n')
+    print('\n', '-' * 36, "Goodbye", '-' * 36, '\n')
+
 
 # Create function to determine length to print each field.
-def fixed_length(text,length):
+def fixed_length(text, length):
     if len(text) > length:
         text = text[:length]
     elif len(text) < length:
         text = (text + " " * length)[:length]
     return text
 
+
 # The main function is called and all of the above code is run.
 main()
 
 # Author: JM Cameron
 # Date: November 2020
-#-----------------------------------------------------------------------------------------------------------------------------------------------------#
+# --------------------------------------------------------------------------------------------------------#
