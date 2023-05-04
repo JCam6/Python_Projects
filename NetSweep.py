22fb2c4c2ae9552b56c72e4bfaa7d45c8ac2ae1f
Jermaine Cameron <jermaine.cameron@gwu.edu>
2023-05-04 12:49:06 -0400

    updated formatting with pyblack

--- NetSweep.py
+++ NetSweep.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     # Print header for program.
-    print('\n', '*' * 10, 'Opening The Program', '*' * 10, '\n')
+    print("\n", "*" * 10, "Opening The Program", "*" * 10, "\n")
 
     # Find gateway / firewall IP.
     os.system("ip route | grep default")
@@ -16,13 +16,19 @@ def main():
     fullrange_gateway_IP = gateway_IP + "/24"
 
     # Save a copy of hosts, ports, and services.
-    background_netlist = os.system('nmap -O -oG networklist.txt "%s" | grep -v "OS:\|TCP"' % (fullrange_gateway_IP))
-    print("\n" + "*" * 10 + "A list of network hosts was saved in networklist.txt." + "*" * 10)
+    background_netlist = os.system(
+        'nmap -O -oG networklist.txt "%s" | grep -v "OS:\|TCP"' % (fullrange_gateway_IP)
+    )
+    print(
+        "\n"
+        + "*" * 10
+        + "A list of network hosts was saved in networklist.txt."
+        + "*" * 10
+    )
 
     # Start repeater to loop through network hosts.
     repeat = "yes"
     while repeat == "yes":
-
         # Wait for network list to be exported.
         Wait = input("\nIs the file in the directory? ")
         if Wait == "yes":
@@ -32,26 +38,35 @@ def main():
 
         # Link to packetstorm for OS version weaknesses.
         print("\n")
-        print("*" * 10 + "Open this link to find potential OS weakesses to fix." + "*" * 10 + "\n")
+        print(
+            "*" * 10
+            + "Open this link to find potential OS weakesses to fix."
+            + "*" * 10
+            + "\n"
+        )
         print("\nhttps://packetstormsecurity.com/files/os/linux/" + "\n")
 
         # Print divider for program.
-        print('\n', '*' * 10, 'This is the list of hosts on the network.', '*' * 10, '\n')
+        print(
+            "\n", "*" * 10, "This is the list of hosts on the network.", "*" * 10, "\n"
+        )
 
         # Run scan of network.
-        os.system('nmap -sn "%s" | grep "report" | cut -d " " -f 5' % (fullrange_gateway_IP))
+        os.system(
+            'nmap -sn "%s" | grep "report" | cut -d " " -f 5' % (fullrange_gateway_IP)
+        )
 
         # Select IP and run service version scan.
         host = input("\nSelect an IP from above for further detection: ")
-        print('\n', '*' * 10, 'This will take about five minutes.', '*' * 10, '\n')
+        print("\n", "*" * 10, "This will take about five minutes.", "*" * 10, "\n")
         os.system('nmap -sV --allports "%s" -v -T4| grep -v "SF"' % (host))
         print("\n")
 
         # Ask to repeat scan for the next host.
         repeat = input("\nEnter yes to continue to the next IP: ")
 
-    # Print footer for program.    
-    print('\n', '*' * 10, 'Closing The Program', '*' * 10, '\n')
+    # Print footer for program.
+    print("\n", "*" * 10, "Closing The Program", "*" * 10, "\n")
 
 
 main()
--- SecureFirst.py
+++ SecureFirst.py
@@ -7,12 +7,16 @@ import os
 
 def main():
     # Print header for program.
-    print('\n', '*' * 10, 'Starting The Program', '*' * 10, '\n')
+    print("\n", "*" * 10, "Starting The Program", "*" * 10, "\n")
 
     # Make backups of the scored service's config files.
-    svc_config_backup = input("\n>>> Create backups of config files? Type y or n, then hit enter:  ")
+    svc_config_backup = input(
+        "\n>>> Create backups of config files? Type y or n, then hit enter:  "
+    )
     if svc_config_backup == "y":
-        svc_name = input("\n>>> What is the scored service? Type openvpn, wordpress, mysql, or docker:  ")
+        svc_name = input(
+            "\n>>> What is the scored service? Type openvpn, wordpress, mysql, or docker:  "
+        )
         if svc_name == "openvpn":
             os.system("cp -r /etc/openvpn /home/openvpn")
             print("\n>>> backups saved to /home/openvpn")
@@ -32,7 +36,9 @@ def main():
         print("\n>>> OK")
 
     # Change password.
-    current_pwd_change = input("\n>>> Change current user's pwd? Type y or n, then hit enter:  ")
+    current_pwd_change = input(
+        "\n>>> Change current user's pwd? Type y or n, then hit enter:  "
+    )
     if current_pwd_change == "y":
         os.system("passwd")
     else:
@@ -45,15 +51,19 @@ def main():
     # Change another user's password.
     user_pwd_repeat = "y"
     while user_pwd_repeat == "y":
-        other_pwd_change = input("\n>>> Change other users pwd? Type y or n, then hit enter:  ")
+        other_pwd_change = input(
+            "\n>>> Change other users pwd? Type y or n, then hit enter:  "
+        )
         if other_pwd_change == "y":
             user_name = input("\n>>> Which user? Type name, then hit enter:  ")
-            os.system('passwd ' "%s" % (user_name))
+            os.system("passwd " "%s" % (user_name))
         else:
             print("\n>>> OK")
 
         # Ask to repeat user password change.
-        user_pwd_repeat = input("\n>>> Enter y to go back and change a user's pwd, enter or n to move on:  ")
+        user_pwd_repeat = input(
+            "\n>>> Enter y to go back and change a user's pwd, enter or n to move on:  "
+        )
 
     # Show users with bash.
     print("\n>>> Here are the users with shell access:\n")
@@ -64,13 +74,15 @@ def main():
         del_user = input("\n>>> Delete a user? Type y or n, then hit enter:  ")
         if del_user == "y":
             user_del_name = input("\n>>> Which user? Type name, then hit enter:  ")
-            os.system('userdel ' "%s" % (user_del_name))
+            os.system("userdel " "%s" % (user_del_name))
             print("\n>>> User " + user_del_name + " deleted sucessfully. ")
         else:
             print("\n>>> OK")
 
         # Ask to repeat user deletetion.
-        user_del_repeat = input("\n>>> Enter y to go back and delete a user, or enter n to move on:  ")
+        user_del_repeat = input(
+            "\n>>> Enter y to go back and delete a user, or enter n to move on:  "
+        )
 
     # Show services running.
     services_show = input("\n>>> Show all services? Type y or n, then hit enter:  ")
@@ -82,25 +94,41 @@ def main():
     # Disable a service.
     service_dis_repeat = "y"
     while service_dis_repeat == "y":
-        service_disable = input("\n>>> Stop / Disable a service (SSH, FTP, RDP, HTTP)? Type y or n, then hit enter:  ")
+        service_disable = input(
+            "\n>>> Stop / Disable a service (SSH, FTP, RDP, HTTP)? Type y or n, then hit enter:  "
+        )
         if service_disable == "y":
-            service_dis_name = input("\n>>> Which service? Type name from above and hit enter:  ")
-            service_cmd = input("\n>>> Does this distro use systemctl? Type y or n, then hit enter:  ")
+            service_dis_name = input(
+                "\n>>> Which service? Type name from above and hit enter:  "
+            )
+            service_cmd = input(
+                "\n>>> Does this distro use systemctl? Type y or n, then hit enter:  "
+            )
             if service_cmd == "y":
-                os.system('systemctl stop ' "%s" % (service_dis_name))
-                os.system('systemctl disable ' "%s" % (service_dis_name))
+                os.system("systemctl stop " "%s" % (service_dis_name))
+                os.system("systemctl disable " "%s" % (service_dis_name))
             else:
-                os.system('stop ' "%s" % (service_dis_name))
-            print("\n>>> Service " + service_dis_name + " stopped / disabled successfully. ")
+                os.system("stop " "%s" % (service_dis_name))
+            print(
+                "\n>>> Service "
+                + service_dis_name
+                + " stopped / disabled successfully. "
+            )
         else:
             print("\n>>> OK")
 
         # Ask to repeat service disable.
-        service_dis_repeat = input("\n>>> Enter y to go back and disable a service, or enter n to move on:  ")
+        service_dis_repeat = input(
+            "\n>>> Enter y to go back and disable a service, or enter n to move on:  "
+        )
 
     # Install packages.
-    print("\nThe following packages will be installed: ufw, fail2ban, net-tools, nmap, and a few more.")
-    package_mgr = input("\n>>> Which package manager is used? Type apt or yum, then hit enter:  ")
+    print(
+        "\nThe following packages will be installed: ufw, fail2ban, net-tools, nmap, and a few more."
+    )
+    package_mgr = input(
+        "\n>>> Which package manager is used? Type apt or yum, then hit enter:  "
+    )
     if package_mgr == "apt":
         os.system("apt-get install nano ufw fail2ban net-tools nmap wget curl -y")
     elif package_mgr == "yum":
@@ -110,7 +138,7 @@ def main():
 
     # Update System.
     sys_update = input("\n>>> Update system? Type y or n, then hit enter:  ")
-    if sys_update =="y":
+    if sys_update == "y":
         if package_mgr == "apt":
             os.system("apt-get update -y")
             os.system("apt-get upgrade -y")
@@ -121,9 +149,9 @@ def main():
             print("\n>>> OK")
     else:
         print("\n>>> OK")
-    
-    # Print footer for program.    
-    print('\n', '*' * 10, 'Closing The Program', '*' * 10, '\n')
+
+    # Print footer for program.
+    print("\n", "*" * 10, "Closing The Program", "*" * 10, "\n")
 
 
 main()
